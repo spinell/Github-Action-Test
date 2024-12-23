0c518e9e83487514f5791fb1c2ba199eee2523cd
spinell <spiner900@gmail.com>
2024-12-23 17:49:32 -0500

    Add cmake (#1)
    - Add cmake files
    - pdate action to run cmake

---
+++ tests/Test1.cpp
@@ -0,0 +1,73 @@
+#include <gtest/gtest.h>
+
+TEST(TestSuiteName, Test1) {
+
+}
+
+TEST(TestSuiteName, Test2) {
+
+}
+
+TEST(TestSuiteName, Test3) {
+
+}
+
+TEST(TestSuiteName, Test4) {
+
+}
+
+TEST(TestSuiteName, Test5) {
+
+}
+
+TEST(TestSuiteName, Test6) {
+
+}
+
+TEST(TestSuiteName, Test7) {
+
+}
+
+TEST(TestSuiteName, Test8) {
+
+}
+
+TEST(TestSuiteName, Test9) {
+
+}
+
+TEST(TestSuiteName_2, Test1) {
+
+}
+
+TEST(TestSuiteName_2, Test2) {
+
+}
+
+TEST(TestSuiteName_2, Test3) {
+
+}
+
+TEST(TestSuiteName_2, Test4) {
+
+}
+
+TEST(TestSuiteName_2, Test5) {
+
+}
+
+TEST(TestSuiteName_2, Test6) {
+
+}
+
+TEST(TestSuiteName_2, Test7) {
+
+}
+
+TEST(TestSuiteName_2, Test8) {
+
+}
+
+TEST(TestSuiteName_2, Test9) {
+
+}
