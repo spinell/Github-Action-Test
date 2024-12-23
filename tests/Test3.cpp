763c15af2b3a8ababa0c78c016830fb8d56cee4d
Luc <spiner900@gmail.com>
2024-12-23 16:07:48 -0500

    add cmake

---
+++ tests/Test3.cpp
@@ -0,0 +1,24 @@
+#include <gtest/gtest.h>
+
+class TestFixtureClassName : public testing::Test {
+ protected:
+  TestFixtureClassName() {
+  }
+};
+
+
+TEST_F(TestFixtureClassName, Test1) {
+  
+}
+
+TEST_F(TestFixtureClassName, Test2) {
+  
+}
+
+TEST_F(TestFixtureClassName, Test3) {
+  
+}
+
+TEST_F(TestFixtureClassName, Test4) {
+  
+}
