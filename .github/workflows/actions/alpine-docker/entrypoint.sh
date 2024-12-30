175d586b2d807144d0f076a4f90247aa8eb91563
Luc <spiner900@gmail.com>
2024-12-30 02:04:01 -0500

    Add docker action example

--- .github/workflows/actions/alpine-docker/entrypoint.sh
+++ .github/workflows/actions/alpine-docker/entrypoint.sh
@@ -13,3 +13,8 @@ ls -la .
 
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
+
+# Right a file inthe container.
+# The file will be available to any subsequent steps in the same job.
+# See: https://docs.github.com/en/actions/sharing-automations/creating-actions/creating-a-docker-container-action#accessing-files-created-by-a-container-action
+echo "Hello from container !" > ./container_output.log
